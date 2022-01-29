af7837041e38551485cbe8a82c8f5269109b7ff2
Artiom Nazarchuk <sel_it@tut.by>
2022-01-29 09:33:33 +0300

    Начало 6-го урока по Laravel

--- resources/views/news/category.blade.php
+++ resources/views/news/category.blade.php
@@ -18,7 +18,6 @@
             <div class="card shadow-sm">
                 <div>
                     <h4>
-                        {{-- Начнём работаь тут обработка моделей и контроллеров--}}
                         <a href="{{ route('news.index') }}"> 
                             <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
                         {{ $newsItem->title }}
