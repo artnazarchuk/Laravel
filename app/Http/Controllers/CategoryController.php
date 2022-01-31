6b1bd076ec23880089e0824f70f89f84b3278793
Artiom Nazarchuk <sel_it@tut.by>
2022-01-31 16:17:54 +0300

    Выполнение ДЗ 6-го урока по Laravel

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -9,10 +9,15 @@ class CategoryController extends Controller
 {
     public function index()
     {
-        $model = new Category();
-        $newsCategory = $model->getNewsCategory();
-        return view('news.category', [
-            'category' => $newsCategory
-        ]);
+        // $model = new Category();
+        // $newsCategory = $model->getNewsCategory();
+        // return view('news.category', [
+            // 'category' => $newsCategory
+        // ]);
+
+        $categories = Category::with('news')->get();
+        return view('news.categories', [
+        'categories' => $categories
+]);
     }
 }
